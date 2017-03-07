ecb40958f65e0a7d1eab4bba922d6a6e633809db
An Nguyen Dang <An Nguyen Dang>
2017-03-07 15:47:56 +0700

    first step for the first function

--- app.js
+++ app.js
@@ -3,11 +3,50 @@
  */
 var express = require('express');
 var app = express();
+var getParameter = require('./function/getParameter');
+var bodyParser = require('body-parser');
+var google = require('./js/ggscript');
+var cmtArray = [];
+app.use(express.static('js'));
 app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', function (req, res) {
-    res.render('index.html');
+    res.redirect('index.html');
+
+})
+
+// facebook and google sheet
+app.post('/fbags', function (req, res) {
+    var postId = getParameter("fbid",req.body.link);
+    res.render("getLink",{postId:postId});
+
+})
+
+app.get("/callback", function(req,res){
+    var code = req.query.code;
+    console.log("request");
+    console.log(req);
+    google.getNewToken(code,function(auth) {
+        google.spreadsheet(auth, cmtArray, function (url) {
+            //console.log(url);
+            res.redirect(url);
+        })
+    });
+})
+// facebook cmt post
+app.post('/fcp', function (req, res) {
+    var cmt = req.body.data;
+    var len = cmt.length;
+    var i=0;
+    cmtArray = [];
+    for (; i<len;i++){
+        cmtArray.push([cmt[i].message]);
+    }
+    google.run();
 })
 //process.env.PORT,process.env.IP
-app.listen(process.env.PORT,process.env.IP, function () {
+app.listen(3000, function () {
     console.log("OK!!");
 })
---
+++ function/getParameter.js
@@ -0,0 +1,14 @@
+function getParameterByName(name, url) {
+    //if (!url) {
+    //    url = window.location.href;
+    //}
+    var name = "" + name;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+module.exports = getParameterByName;
