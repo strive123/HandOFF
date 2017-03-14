9b5188174ff09bba1ed6de71b964f3e9a7d40146
An Nguyen Dang <An Nguyen Dang>
2017-03-14 09:49:34 +0700

    first commit

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1228873080533218',
+        'clientSecret'  : '594d36c47a61140738c0b39212be34b0', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '145906865326-p3bdc48dreas6ip9ptdte0acb7f9oouj.apps.googleusercontent.com',
+        'clientSecret'  : 'NAvQGCwV5wz0wViI0Akf_-RS',
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Admin on 3/12/2017.
+ */
+module.exports = {
+    'url' : 'mongodb://dangan:dangan@ds129600.mlab.com:29600/handoff'
+};
